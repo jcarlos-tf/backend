12c8f5e40ef6ce40183a5b0aebf3162fd793f1d5
jcarlos-tf <juancarlostf51@gmail.com>
2020-10-19 18:26:39 -0500

    cambios agregados

--- src/routes/v1/favorites-routes.js
+++ src/routes/v1/favorites-routes.js
@@ -7,3 +7,4 @@ router.post("/create", favoritesController.createFavorites);
 router.get("/get-favorites", favoritesController.getFavorites);
 
 module.exports = router;
+//cambios agregados ejemplos
